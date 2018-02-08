9ca799d426329b26b73dfa0db316a7eb6e40cfc5
Matthew Carr <matthewcarr47@gmail.com>
2018-02-07 21:48:38 -0800

    "extension for about me page"

--- js/app.js
+++ js/app.js
@@ -1,79 +1,104 @@
 'use strict';
 
+var count = 0;
 var userName = prompt('Hi! Glad you are here! What\'s your name?');
 
 alert('Hi, ' + userName +   ' nice to meet you! Im going to ask you a few questions so you can get to know me. Please answer with y/n or yes/no');
 
-var answer1 = prompt('Do you think I own a dog?').toLowerCase().trim();
-console.log('the user put ' + answer1 + ' for if i have a dog.');
+var answer = prompt('Do you think I own a dog?').toLowerCase().trim();
+console.log('the user put ' + answer + ' for if i have a dog.');
 
-if (answer1 === 'yes'|| answer1 === 'y') {
+if (answer === 'yes'|| answer === 'y') {
   alert('You ' + userName + ' are correct! I have a Corgi named Butters');
-} else if (answer1 === 'Um, no' || answer1 === 'n') {
+  count++;
+} else if (answer === 'Um, no' || answer === 'n') {
   alert('Sorry ' + userName + ' you\'re incorrect, I have a Corgi named Butters!');
 } else {
   alert('Sorry that was not a valid answer. Maybe you made a typo? Lets move on to the next question.');
 }
 
-var answer2 = prompt('Next question! Do you think I have a brother?').toLowerCase().trim();
-console.log('the user put ' + answer2 + ' for if I have a brother.');
+answer = prompt('Next question! Do you think I have a brother?').toLowerCase().trim();
+console.log('the user put ' + answer + ' for if I have a brother.');
 
-if (answer2 === 'no' || answer2 === 'n') {
+if (answer === 'no' || answer === 'n') {
   alert('You\'re incorrect, ' + userName + ' I have 2 brothers');
-} else if (answer2  === 'yes'|| answer2 === 'y') {
+} else if (answer  === 'yes'|| answer === 'y') {
   alert('Correct, I have two brothers!');
+  count++;
 } else {
   alert('Invalid Response, but let\'s move on.');
 }
 
-var answer3 = prompt('Do you think I\'ve spent my entire life in Washington?').toLowerCase().trim();
-console.log('the user put ' + answer3 + ' for whether I\'ve spent my entire life in Washington or not.');
+answer = prompt('Do you think I\'ve spent my entire life in Washington?').toLowerCase().trim();
+console.log('the user put ' + answer + ' for whether I\'ve spent my entire life in Washington or not.');
 
-if (answer3 === 'yes' || answer3 === 'y') {
+if (answer === 'yes' || answer === 'y') {
   alert('I\'m sorry, but ' + userName + ' you\'re wrong, as an army brat I\'ve moved around a lot.');
-} else if (answer3 === 'no' || answer3 === 'n') {
+} else if (answer === 'no' || answer === 'n') {
   alert('You are correct ' + userName + ' as an Army brat my family and I moved just about every two years');
+  count++;
 } else {
   alert('Sorry that response is not valid, but let\'s just continue.');
 }
 
-var answer4 = prompt('Do you think I enjoy playing golf?').toLowerCase().trim();
-console.log('the user put ' + answer4 + ' for whether I enjoy playing golf or not.' );
+answer = prompt('Do you think I enjoy playing golf?').toLowerCase().trim();
+console.log('the user put ' + answer + ' for whether I enjoy playing golf or not.' );
 
-if (answer4 === 'yes' || answer4 === 'y') {
+if (answer === 'yes' || answer === 'y') {
   alert('You ' + userName + ' are right, I\'ve been golfing for over twenty years.');
-} else if (answer4 === 'no' || answer4 === 'n') {
+  count++;
+} else if (answer === 'no' || answer === 'n') {
   alert('Sorry you\'re wrong, I\'ve golfed for over twenty years!');
 } else {
   alert('I\'m sorry ' + userName + ' invalid answer, let\'s just continue.');
 }
 
-var answer5 = prompt('Do you think I would drive every day to class?').toLowerCase().trim();
-console.log('the user put ' + answer5 + ' for whether I drive to class or not.');
+answer = prompt('Do you think I would drive every day to class?').toLowerCase().trim();
+console.log('the user put ' + answer + ' for whether I drive to class or not.');
 
-if (answer5 === 'yes' || answer5 === 'y') {
+if (answer === 'yes' || answer === 'y') {
   alert('Sorry ' + userName + ' I couldn\'t justify spending that much on parking. I bus in from Issaquah.');
-} else if (answer5 === 'no' || answer5 === 'n') {
+} else if (answer === 'no' || answer === 'n') {
   alert('You ' + userName + ' are actually correct, I could never justify spending that much money on parking');
+  count++;
 } else {
   alert('Sorry ' + userName + ' you\'ve enterd an invalid response, but thank you for playing my guessing game.');
 }
-var rightNum = 4;
 
-for(var i = 1; i <= 4; i++) {
-  var answer6 = prompt('Hey ' + userName + ' lets play a little game. I would like you to guess a number from 1 to 10. I\'ll give you 4 chances.');
-  var num = parseInt(answer6);
+
+var rightNum = 4; //variable rightNum is used to store the answer for the guessing game.
+
+for (var i = 1; i <= 4; i++) {
+  answer = prompt('Hey ' + userName + ' lets play a little game. I would like you to guess a number from 1 to 10. I\'ll give you 4 chances.');
+  var num = parseInt(answer); // Variable 'num' used to store the answer given by user in prompt box. parseInt store what the user has entered.
+  console.log('The user input ' + num + ' as a guess to what number I selected');
   
-  var remain = 4 - i;
-
-  if(num > rightNum) {
-    alert('Sorry ' + userName + ' your guess is too high ' + remain.toString() + ' guesses left.');
-  } else if (num < rightNum) {
-    alert('Sorry ' + userName + ' your guess is too low ' + remain.toString() + ' guesses left.');
+  if (num > rightNum) { // If answer given is greater than rightNum(4).
+    alert('Sorry ' + userName + ' your guess is too high ' + (4 - i) + ' guesses left.'); //remain.toString().Converts remaining guesses after each loop
+  } else if (num < rightNum) { // If answer given is less than rightNum(4).
+    alert('Sorry ' + userName + ' your guess is too low ' + (4 - i) + ' guesses left.');
   } else { 
-    alert('You are correct!');
+    alert('Great guess ' + userName + ' you are correct!');
+    count++;
     break;
-  
   }
 }
+
+
+var states = ['virginia', 'alabama'];
+
+for (var i = 0; i < 6; i++) {
+  answer = prompt('Alright ' + userName + ' last question I promise. Can you guess which states I\'ve lived in besides Washington?').toLowerCase().trim();
+  console.log('The user put ' + answer + ' for try number ' + (1 + i));
   
+  if (answer === states[0] || answer === states[1]) {
+    alert('You ' + userName + ' are correct, the answers were Virginia and Alabama');
+    count++;
+    break; 
+  } else if (i === 5) {
+    alert('I\'m so sorry you\'ve run out of guesses ' + userName + '.' );
+  } else {
+    alert('Sorry ' + userName + ' you are incorrect. You have ' + (5 - i) + ' amount of tries left');
+  }       
+}
+alert('Well done ' + userName + ' you got ' + count + ' out of 7 questions right? Thanks for playing?' );
