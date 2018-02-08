b03f4d38677a37592de685c2cbe85b8b63faa65e
Matthew Carr <matthewcarr47@gmail.com>
2018-02-08 13:53:53 -0800

    made corrections to code syntax

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ alert('Hi, ' + userName +   ' nice to meet you! Im going to ask you a few questi
 
 var answer = prompt('Do you think I own a dog?').toLowerCase().trim();
 console.log('the user put ' + answer + ' for if i have a dog.');
-
+/*
 if (answer === 'yes'|| answer === 'y') {
   alert('You ' + userName + ' are correct! I have a Corgi named Butters');
   count++;
@@ -64,41 +64,46 @@ if (answer === 'yes' || answer === 'y') {
 } else {
   alert('Sorry ' + userName + ' you\'ve enterd an invalid response, but thank you for playing my guessing game.');
 }
-
+*///
 
 var rightNum = 4; //variable rightNum is used to store the answer for the guessing game.
 
-for (var i = 1; i <= 4; i++) {
+for (var i = 0; i < 4; i++) {
   answer = prompt('Hey ' + userName + ' lets play a little game. I would like you to guess a number from 1 to 10. I\'ll give you 4 chances.');
   var num = parseInt(answer); // Variable 'num' used to store the answer given by user in prompt box. parseInt store what the user has entered.
   console.log('The user input ' + num + ' as a guess to what number I selected');
   
   if (num > rightNum) { // If answer given is greater than rightNum(4).
-    alert('Sorry ' + userName + ' your guess is too high ' + (4 - i) + ' guesses left.'); //remain.toString().Converts remaining guesses after each loop
+    alert('Sorry ' + userName + ' your guess is too high ' + (4 - (1 + i)) + ' guesses left.'); //remain.toString().Converts remaining guesses after each loop
   } else if (num < rightNum) { // If answer given is less than rightNum(4).
-    alert('Sorry ' + userName + ' your guess is too low ' + (4 - i) + ' guesses left.');
-  } else { 
+    alert('Sorry ' + userName + ' your guess is too low ' + (4 - (i + 1)) + ' guesses left.');
+  } else if(num === rightNum) { 
     alert('Great guess ' + userName + ' you are correct!');
     count++;
     break;
-  }
+  } else {
+    alert('You have entered an invalid response!');
+  } 
 }
 
 
-var states = ['virginia', 'alabama'];
+// questio6() {
+var states = ['virginia', 'alabama']; //array of correct answers.
 
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < 6; i++) { //Counter starts at (i) zero, and stops at 6, incrementing by one each iteration of the loop.
   answer = prompt('Alright ' + userName + ' last question I promise. Can you guess which states I\'ve lived in besides Washington?').toLowerCase().trim();
-  console.log('The user put ' + answer + ' for try number ' + (1 + i));
+  console.log('The user put ' + answer + ' for try number ' + (1 + i)); //log the answer for each guess,  incrementing  the guess by one for each entry to guess prompt.
   
-  if (answer === states[0] || answer === states[1]) {
-    alert('You ' + userName + ' are correct, the answers were Virginia and Alabama');
-    count++;
-    break; 
-  } else if (i === 5) {
-    alert('I\'m so sorry you\'ve run out of guesses ' + userName + '.' );
+  if (answer === states[0] || answer === states[1]) { //If answer is in array index, either [0] or [1].
+    alert('You ' + userName + ' are correct, the answers were Virginia and Alabama'); // response to correct answer
+    count++; // logs correct answers!
+    break; //exit loop if conditions are met.
+  } else if (i === 5) { // If user is out of guesses. Count will reach 6 attempts here.
+    alert('I\'m so sorry you\'ve run out of guesses ' + userName + '.' ); // Notify of reaching max guesses.
   } else {
-    alert('Sorry ' + userName + ' you are incorrect. You have ' + (5 - i) + ' amount of tries left');
+    alert('Sorry ' + userName + ' you are incorrect. You have ' + (5 - i) + ' amount of tries left');// Condition run for each wrong answer.
   }       
 }
-alert('Well done ' + userName + ' you got ' + count + ' out of 7 questions right? Thanks for playing?' );
+// }
+// question6();
+alert('Well done ' + userName + ' you got ' + count + ' out of 7 questions right? Thanks for playing?' ); //Final alert. String built to store the count of correct answers VS number of questions.
